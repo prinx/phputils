c57e8d34e03623b75623a1833c75c231105e771d
CinPer <damien.dorcis@yahoo.com>
2020-10-01 10:47:32 +0000

    Remove default connection option persistent

--- src/DB.php
+++ src/DB.php
@@ -21,27 +21,26 @@ class DB
     /**
      * Returns a PDO connection to the database
      *
-     * @param array $params
-     * @param array $option
-     * @return \PDO
+     * @param  array      $params
+     * @param  array      $option
      * @throws \Exception If unable to connect to the database
+     * @return \PDO
      */
-    public static function load(array $params, array $options = [
-        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-        \PDO::ATTR_PERSISTENT => true,
-    ]) {
+    public static function load(
+        array $params, array $options = [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]
+    ) {
 
         $required = ['dbname' /* 'driver', 'host', 'port', 'user', 'password' */];
         foreach ($required as $paramName) {
             if (!isset($params[$paramName])) {
-                throw new \Exception('Parameter ' . $paramName . ' is required to connect to the database.');
+                throw new \Exception('Parameter '.$paramName.' is required to connect to the database.');
             }
         }
 
         $dsn = $params['driver'] ?? 'mysql';
-        $dsn .= ':host=' . $params['host'] ?? 'localhost';
-        $dsn .= ';port=' . $params['port'] ?? 3306;
-        $dsn .= ';dbname=' . $params['dbname'];
+        $dsn .= ':host='.$params['host'] ?? 'localhost';
+        $dsn .= ';port='.$params['port'] ?? 3306;
+        $dsn .= ';dbname='.$params['dbname'];
 
         $user = $params['user'] ?? '';
         $pass = $params['password'] ?? '';
@@ -49,25 +48,25 @@ class DB
         try {
             return new \PDO($dsn, $user, $pass, $options);
         } catch (\PDOException $e) {
-            throw new \PDOException('ERROR CONNECTING TO DATABASE: ' . $e->getMessage() . ' in file ' . $e->getFile() . ':' . $e->getLine());
+            throw new \PDOException('ERROR CONNECTING TO DATABASE: '.$e->getMessage().' in file '.$e->getFile().':'.$e->getLine());
         }
     }
 
     /**
      * Create an INSERT query string
      *
-     * @param string $tableName
-     * @param array $fields
+     * @param  string   $tableName
+     * @param  array    $fields
      * @return string
      */
     public static function createInsertSqlString($tableName, $fields)
     {
-        $fields_str = "";
-        $values_str = "";
+        $fields_str = '';
+        $values_str = '';
 
         foreach ($fields as $value) {
-            $fields_str .= $value . ", ";
-            $values_str .= ":" . $value . ", ";
+            $fields_str .= $value.', ';
+            $values_str .= ':'.$value.', ';
         }
 
         $fields_str = rtrim($fields_str, ', ');
@@ -79,10 +78,10 @@ class DB
     /**
      * Insert a row to a table of the database
      *
-     * @param array $data Associative array mapping the columns names of the table to the values that will inserted
-     * @param string $tableName
-     * @param \PDO $db
-     * @return int The ID of the inserted row
+     * @param  array  $data         Associative array mapping the columns names of the table to the values that will inserted
+     * @param  string $tableName
+     * @param  \PDO   $db
+     * @return int    The ID of the inserted row
      */
     public static function insert($data, $tableName, $db)
     {
@@ -98,9 +97,9 @@ class DB
      * Parse a database result replacing integers by real integers
      * By default, the database return everything as string
      *
-     * @param array $arr
-     * @return array
      * @todo Support for object (sometimes the response is an object instead of array)
+     * @param  array   $arr
+     * @return array
      */
     public static function resultWithTrueInt($arr)
     {
@@ -116,11 +115,11 @@ class DB
     /**
      * I wonder the necessity of this function
      *
-     * @param array $data
+     * @param  array    $data
      * @return string
      */
     public static function createWhereInRange(array $data)
     {
-        return '(' . implode(',', $data) . ')';
+        return '('.implode(',', $data).')';
     }
 }
